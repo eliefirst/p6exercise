f9bccacbc40d5787ac8077e8c8079b28775f3925
elie <elie@redline-univers.com>
2021-10-04 03:20:27 +0200

    gestion images

--- go-fullstack/backend/app.js
+++ go-fullstack/backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require ('./routes/user');
 
 mongoose.connect('mongodb+srv://elie:ut4h0us123@cluster0.oqvpj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {
@@ -23,6 +25,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes);
-
+app.use('/api/auth', userRoutes);
 module.exports = app;
--- go-fullstack/backend/controllers/stuff.js
+++ go-fullstack/backend/controllers/stuff.js
@@ -2,9 +2,11 @@ const Thing = require('../models/Thing');
 const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
-    delete req.body._id;
+    const thingObjet = JSON.parse(req.body.thing);
+    delete thingObjet._id;
     const thing = new Thing({
-        ...req.body
+        ...thingObjet,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     thing.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
@@ -12,17 +14,22 @@ exports.createThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-};
+    const thingObject = req.file ?
+      {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      } : { ...req.body };
+    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  };
 
 exports.deleteThing = (req, res, next) => {
     Thing.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
         .catch(error => res.status(400).json({ error }));
 };
-       
+
 
 exports.getOneThing = (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
